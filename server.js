bcb15e80cc1bbdfb463ad654ab0b10aca21e6551
Patrick Du <patrickduu@gmail.com>
2021-02-20 17:59:14 -0500

    Update server CORS origin

--- server.js
+++ server.js
@@ -9,12 +9,13 @@ dotenv.config();
 
 // Environment Variables
 const PORT = process.env.PORT || 5000;
+const FRONTEND_URI = process.env.FRONTEND_URI || 'https://localhost:3000';
 
 // Initialize Express App
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({ origin: FRONTEND_URI, credentials: true }));
 app.use(morgan('dev'));
 app.use(express.json());
 
@@ -22,4 +23,5 @@ app.use(express.json());
 app.use(require('./routes'));
 
 // Server
+/* eslint-disable no-console */
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
